01ba81a4990909a7fb2c9677f353c034221d560a
Adrian Carmiol <acarmiolu@ucenfotec.ac.cr>
2017-03-20 14:47:01 -0600

    example of socket.io with angular

---
+++ 03_chat/public/main/main.Ctrl.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('MainCtrl', MainCtrl);
+
+  MainCtrl.$inject = ['$scope', '$localStorage', 'socket', 'lodash'];
+
+  function MainCtrl($scope, $localStorage, socket, lodash) {
+  	$scope.mynickname = $localStorage.nickname;
+
+  };
+})();
---
+++ 03_chat/server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var port = 8080;
+var users = [];
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on('connection', function(socket) {
+  console.log('new connection made');
+
+  // When new socket joins
+  socket.on('join',function(data){
+  	console.log(data);
+  	console.log(users);
+  	socket.nickname = data.nickname;
+  	users[socket.nickname]=socket;
+  	var userObj = {
+  		nickname:data.nickname,
+  		socketid:socket.id
+  	}
+  	users.push(userObj);
+  	io.emit('all-users', users)
+  });
+
+});
+
+server.listen(port, function() {
+  console.log("Listening on port " + port);
+});
